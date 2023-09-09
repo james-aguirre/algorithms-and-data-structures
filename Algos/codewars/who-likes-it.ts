bbe00b675bdbf473176a5d753592a69c97f58b4f
James Aguirre <jamesaguirre98@gmail.com>
2023-09-08 23:10:25 -0700

    switched solutions to ts

--- Algos/codewars/who-likes-it.ts
+++ Algos/codewars/who-likes-it.ts
@@ -6,11 +6,11 @@
 ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 ["Alex", "Jacob", "Mark", "Max"] --> "Alex, Jacob and 2 others like this"
  */
-const whoLikesIt = (names) => {
+const whoLikesIt = (names: string[]) => {
   if (names.length === 0) return `no one likes this`;
   if (names.length === 1) return `${names[0]} likes this`;
   if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
   if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
   if (names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-  return likes;
+  return [];
 };
