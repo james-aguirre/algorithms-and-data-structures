447b72a5d65b94daa58e395ad9fa86be07baee59
James Aguirre <jamesaguirre98@gmail.com>
2023-09-08 23:06:55 -0700

    switched solutions to ts

--- Algos/codewars/two-lowest-sum.ts
+++ Algos/codewars/two-lowest-sum.ts
@@ -6,7 +6,7 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
  */
 
-function sumTwoSmallestNumbers(numbers) {
-  var [a, b] = numbers.sort((a, b) => a - b);
+function sumTwoSmallestNumbers(numbers: number[]): number {
+  const [a, b] = numbers.sort((a, b) => a - b);
   return a + b;
 }
