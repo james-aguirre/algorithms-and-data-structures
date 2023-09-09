813d95bc7cd35d40a99081b8c66cdf314a11a626
James Aguirre <jamesaguirre98@gmail.com>
2023-09-08 23:18:26 -0700

    switched solutions to ts

--- Algos/codewars/who-likes-it.ts
+++ Algos/codewars/who-likes-it.ts
@@ -12,5 +12,4 @@ const whoLikesIt = (names: string[]) => {
   if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
   if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
   if (names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-  return [];
 };
