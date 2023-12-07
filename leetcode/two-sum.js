69bceb7ff3655af335d9d566c98fcc01e83435a9
James Aguirre <jamesaguirre98@gmail.com>
2023-12-07 14:31:08 -0800

    alternate solution

--- leetcode/two-sum.js
+++ leetcode/two-sum.js
@@ -26,3 +26,15 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+// alternate solution, very fast runtime for much worse space complexity
+var fasterTwoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
